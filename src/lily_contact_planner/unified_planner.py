2facfe6cb6eb9978fbf020cf390b7248d9cf9436
YutoRobo <49127327+YutoRobo@users.noreply.github.com>
2026-08-15 16:04:47 +0900

    Use horizon-selectable v005 recovery

--- src/lily_contact_planner/unified_planner.py
+++ src/lily_contact_planner/unified_planner.py
@@ -10,14 +10,14 @@ from .planner_touchdown import TouchdownSearchMixin
 from .staged_search_v006 import V006StagedSearchMixin
 from .prm_recovery import PRMStaticRecoveryMixin
 from .static_prm_candidates import StaticPRMCandidateMixin
-from .multi_contact_v005 import V005MultiRecoveryMixin
+from .staged_multi_v005 import V005StagedHorizonMixin
 from .v004_success_seed import V004SuccessfulSeedMixin
 
 
 class UnifiedContactPlanner(
     StaticPRMCandidateMixin,
     PRMStaticRecoveryMixin,
-    V005MultiRecoveryMixin,
+    V005StagedHorizonMixin,
     V004SuccessfulSeedMixin,
     V006StagedSearchMixin,
     TouchdownSearchMixin,
